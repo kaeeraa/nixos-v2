45b6cebefaf6b5ed5c9aefe3395ce871f014efde
kaeeraa <kaeeraa@nebula-nook.ru>
2026-05-14 17:00:37 +0300

    feat: add git-signing key

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,16 +1,24 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  keyFile = "/var/lib/sops-nix/age.key";
+in {
+  environment.variables = {"SOPS_AGE_KEY_FILE" = keyFile;};
   environment.systemPackages = with pkgs; [sops age-plugin-tpm age];
   sops = {
     defaultSopsFile = ./secrets.yaml;
 
     age = {
       plugins = [pkgs.age-plugin-tpm];
-      keyFile = "/var/lib/sops-nix/age.key";
+      keyFile = keyFile;
     };
 
     secrets = {
       github = {
         path = "/home/kaeeraa/.ssh/github.ed25519";
+        owner = "kaeeraa";
+        mode = "0600";
+      };
+      git-signing = {
+        path = "/home/kaeeraa/.ssh/git-signing.ed25519";
         owner = "kaeeraa";
         mode = "0600";
       };
